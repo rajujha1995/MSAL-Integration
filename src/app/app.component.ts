a866864e4e772816acb1c8781dc64480bf0624e6
A2645550 <raj1995.rj@gmail.com>
2023-06-30 22:24:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div *ngIf="isAuthenticated">
+      <h1>Welcome, {{ userDisplayName }}</h1>
+      <button (click)="logout()">Logout</button>
+    </div>
+    <div *ngIf="!isAuthenticated">
+      <button (click)="login()">Login</button>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  isAuthenticated = false;
+  userDisplayName = '';
+
+  constructor(private authService: MsalService) {}
+
+  login() {
+    this.authService.loginRedirect()
+      .subscribe(() => {
+        this.isAuthenticated = this.authService.instance.getAllAccounts().length > 0;
+        if (this.isAuthenticated) {
+          const account = this.authService.instance.getAllAccounts()[0];
+          this.userDisplayName = account.name ? account.name : 'Unknown User';
+        }
+      });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isAuthenticated = false;
+    this.userDisplayName = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { MsalModule, MsalInterceptor, MSAL_INSTANCE,MsalService } from '@azure/msal-angular';
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// Replace the placeholders with your actual values
+const clientId = 'YOUR_CLIENT_ID';
+const authority = 'https://login.microsoftonline.com/YOUR_TENANT_ID';
+
+function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: clientId,
+      authority: authority,
+      redirectUri: window.location.origin,
+    },
+    cache: {
+      cacheLocation: 'localStorage',
+      storeAuthStateInCookie: false,
+    },
+  });
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MsalModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    MsalService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
